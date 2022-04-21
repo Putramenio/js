afe0cfecbc3a838ea6d914717c5f310dcaef0eca
Antoni Kukawka <79514935+Putramenio@users.noreply.github.com>
2022-04-21 16:37:35 +0300

    Add files via upload

---
+++ komis/auto.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>komis samochodowy </title>
+        <link rel="stylesheet" type="text/css" href="auto.css">
+        </head>
+        <body>
+    <header>
+        <h1> SAMOCHODY</h1>
+    </header>
+    <div id="lewy">
+         <h2>Wykaz samochodow</h2>
+         <ul>
+             <?php //script1 ?>
+         </ul>
+         <h2>Zamowienia</h2>
+         <ul>
+             <?php //script1 ?>
+         </ul>
+</div>
+    <div id="prawy">
+        <h2>Pelne dane: Fiat</h2>
+        <ul>
+             <?php //script3 ?>
+         </ul>
+    </div>
+    <footer> 
+        <table> 
+            <tr>
+                <td>
+                    <a href="kwerendy.txt">Kwerendy</a>
+                </td>
+                <td>Autor: 12345678910</td>
+                <td>
+                    <img src="auto.png" alt="komis samochodowy">
+                </td>
+            </tr>
+        </table>
+      </footer>
+    </body>
+</html>
+
